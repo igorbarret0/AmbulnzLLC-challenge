a3a6df856ab6bdd4e97f83fa3b7685df358f2c7f
Pichau <igorof06@outlook.com>
2024-10-03 21:35:48 -0300

    feat: starting project

---
+++ AmbulnzLLC-challenge/src/main/java/com/barreto/AmbulnzLLC_challenge/AmbulnzLlcChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.barreto.AmbulnzLLC_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbulnzLlcChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbulnzLlcChallengeApplication.class, args);
+	}
+
+}
---
+++ AmbulnzLLC-challenge/src/test/java/com/barreto/AmbulnzLLC_challenge/AmbulnzLlcChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barreto.AmbulnzLLC_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbulnzLlcChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
